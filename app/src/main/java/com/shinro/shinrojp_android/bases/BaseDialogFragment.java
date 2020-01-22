3992a3bbc800e6e63b831b622ce05a9abaff5f52
fox <itachi1611@gmail.com>
2020-01-23 00:05:26 +0700

    Update BaseDialogFragment

--- app/src/main/java/com/shinro/shinrojp_android/bases/BaseDialogFragment.java
+++ app/src/main/java/com/shinro/shinrojp_android/bases/BaseDialogFragment.java
@@ -1,4 +1,17 @@
 package com.shinro.shinrojp_android.bases;
 
-public class BaseDialogFragment {
+import androidx.fragment.app.DialogFragment;
+
+import io.reactivex.disposables.CompositeDisposable;
+
+public class BaseDialogFragment extends DialogFragment {
+
+    private CompositeDisposable disposable = new CompositeDisposable();
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        disposable.dispose();
+    }
+
 }
